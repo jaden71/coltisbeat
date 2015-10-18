956f57ddc63fd4a3fce498fb16b9b1a3259e0940
Jaden Wang <jadenyjw@gmail.com>
2015-10-18 20:22:37 +0000

    static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Colt is Beat"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Colt is Beat"
+  end
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Colt is Beat"
   end
 
 end
