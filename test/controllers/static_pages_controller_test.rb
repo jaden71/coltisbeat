c1ebbf67eb2ba9bbd7206231edd253eb78f16b34
Jaden Wang <jadenyjw@gmail.com>
2015-10-18 22:56:36 +0000

    full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Colt is Beat"
+    assert_select "title", "Colt is Beat"
   end
 
   test "should get help" do
